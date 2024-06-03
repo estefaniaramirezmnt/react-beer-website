0255da28ae7734ea286601f6fe59db64e282452c
Estefanía <estefaniaramirezmnt@gmail.com>
2024-06-03 18:27:11 +0200

    Fetch data using suspense
    Created FetchData.js and wrape the data inside Suspense

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import Header from './components/Header'
+import React, { Suspense } from "react";
+import Header from "./components/Header";
+import fetchData from "./components/FetchData";
 
+import "./App.css";
+
+const apiData = fetchData("https://api.sampleapis.com/beers/ale");
+
+function BeerList() {
+  const data = apiData.read();
+  return (
+    <ul className="beers">
+      {data?.map((beer) => (
+        <li key={beer.id}>
+          <h3>{beer.name}</h3>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 function App() {
   return (
     <div>
       <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BeerList />
+      </Suspense>
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
