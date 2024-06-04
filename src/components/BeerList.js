2735bd753e9d4139dff7b6f9b7d39ccb82a64d93
Estefanía <estefaniaramirezmnt@gmail.com>
2024-06-04 10:30:26 +0200

    Added images to beer list
    Added images wrapped inside Boostrap rows and columns

--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -1,6 +1,12 @@
 import { useState, useContext } from "react";
-import ReactPaginate from 'react-paginate';
+import ReactPaginate from "react-paginate";
 import { ApiContext } from "./ApiContext";
+// import boostrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
 
 function BeerList() {
   const apiData = useContext(ApiContext);
@@ -9,7 +15,7 @@ function BeerList() {
 
   const data = apiData.read();
   data.sort((a, b) => a.name.localeCompare(b.name));
-  
+
   const offset = pageNumber * beersPerPage;
   const currentPageData = data.slice(offset, offset + beersPerPage);
 
@@ -21,23 +27,36 @@ function BeerList() {
 
   return (
     <div>
-      <ul className="beers">
+      {/* <ul className="beers">
         {currentPageData?.map((beer) => (
           <li key={beer.id}>
             <h3>{beer.name}</h3>
+            <img src={beer.image} alt={beer.name} />
           </li>
         ))}
-      </ul>
+      </ul> */}
+      {/* Display the data with rows and columns with boostrap */}
+
+      <Container className="beer-container">
+        <Row>
+          {currentPageData?.map((beer) => (
+            <Col key={beer.id} md={3} className="individual-beer">
+              <h3>{beer.name}</h3>
+              <Image src={beer.image} alt={beer.name} fluid />
+            </Col>
+          ))}
+        </Row>
+      </Container>
       <ReactPaginate
-        previousLabel={'previous'}
-        nextLabel={'next'}
-        breakLabel={'...'}
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
         pageCount={pageCount}
         marginPagesDisplayed={2}
         pageRangeDisplayed={5}
         onPageChange={handlePageChange}
-        containerClassName={'pagination'}
-        activeClassName={'active'}
+        containerClassName={"pagination"}
+        activeClassName={"active"}
       />
     </div>
   );
