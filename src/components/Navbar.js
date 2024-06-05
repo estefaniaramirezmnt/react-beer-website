92707bee98df4bce7b7f5540b830ed8d113b5877
Estefanía <estefaniaramirezmnt@gmail.com>
2024-06-05 13:53:18 +0200

    Changed again navbar style
    Change navbar styles again because previous selection of colours had a
    conflict with boostrap selectors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../style/navbar.css";
@@ -5,13 +6,13 @@ import "../style/navbar.css";
 function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <NavLink className="navbar-brand mr-4" to="/">
             Don't worry, beer happy!
           </NavLink>
           <button
-            className="navbar-toggler"
+            className="myNavbarToggler navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
@@ -19,22 +20,35 @@ function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="myToggle navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <NavLink className={`nav-link ${({ isActive }) => (isActive ? "active" : "")}`} exact to="/">
+                <NavLink
+                  className={`nav-link ${({ isActive }) =>
+                    isActive ? "active" : ""}`}
+                  exact
+                  to="/"
+                >
                   Home
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className={`nav-link ${({ isActive }) => (isActive ? "active" : "")}`} to="/beerlist">
+                <NavLink
+                  className={`nav-link ${({ isActive }) =>
+                    isActive ? "active" : ""}`}
+                  to="/beerlist"
+                >
                   Beer List
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className={`nav-link ${({ isActive }) => (isActive ? "active" : "")}`} to="/findyourbeer">
+                <NavLink
+                  className={`nav-link ${({ isActive }) =>
+                    isActive ? "active" : ""}`}
+                  to="/findyourbeer"
+                >
                   Find your beer
                 </NavLink>
               </li>
@@ -47,3 +61,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
