3a32b290db6013822568a56400075077f51046c8
Estefanía <estefaniaramirezmnt@gmail.com>
2024-06-06 12:20:47 +0200

    Made smaller image gallery

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,42 @@
 import React from "react";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
+
 import coldBeer from "../img/coldBeer.jpg";
 import closeViewBeer from "../img/closeViewBeer.jpg";
 import beerToast from "../img/beerToast.jpg";
 import severalBeers from "../img/severalBeers.jpg";
 import beerAndBread from "../img/beerAndBread.jpg";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
 function Home() {
+  const images = [
+    { 
+      original: coldBeer,
+      thumbnail: coldBeer,
+    },
+    { original: closeViewBeer,
+      thumbnail: closeViewBeer,
+     },
+    { original: beerToast,
+      thumbnail: beerToast
+     },
+    { original: severalBeers,
+      thumbnail: severalBeers
+     },
+    { original: beerAndBread,
+      thumbnail: beerAndBread
+     },
+  ];
+
   return (
     <div className="home-container">
-      <h3 className="home-title">A page dedicated to beer lovers!</h3>
-      <div className="home-img-container">
-            <img src={coldBeer} alt="Cold beer" className="coldBeer1" />
-            <img src={closeViewBeer} alt="Cold beer" className="closeViewBeer" />
-            <img src={beerToast} alt="Friends in a field toasting with beers" className="beerToast" />
-            <img src={severalBeers} alt="Different beers in a table" className="severalBeers" />
-            <img src={beerAndBread} alt="Beer and bread" className="bearAndBread" />
-      </div>
+      <h3 className="home-title">A PAGE DEDICATED TO BEER LOVERS!</h3>
+      <ImageGallery items={images} 
+        showPlayButton={true}
+        showFullscreenButton={false}
+        slideInterval={4000}
+        autoPlay={true}
+      />
     </div>
   );
 }
