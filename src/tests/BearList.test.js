acbbe34906e26abdcd0d048a4c20afc0ef09c1f3
Estefanía <estefaniaramirezmnt@gmail.com>
2024-06-06 16:42:53 +0200

    Created BeerList.text.js and written mock data

---
+++ src/tests/BearList.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import BeerList from "../components/BeerList";
+import { ApiContext } from "../components/ApiContext";
+
+const mockApiData = [
+  {
+    id: 1,
+    name: "Ale Beer",
+    image: "ale.jpg",
+    alt: "Ale Beer",
+    price: "$5.00",
+  },
+  {
+    id: 2,
+    name: "Lager Beer",
+    image: "lager.jpg",
+    alt: "Lager Beer",
+    price: "$4.00",
+  },
+  {
+    id: 3,
+    name: "Pilsner Beer",
+    image: "pilsner.jpg",
+    alt: "Pilsner Beer",
+    price: "$6.00",
+  },
+  {
+    id: 4,
+    name: "Stout Beer",
+    image: "stout.jpg",
+    alt: "Stout Beer",
+    price: "$7.00",
+  },
+  {
+    id: 5,
+    name: "Wheat Beer",
+    image: "wheat.jpg",
+    alt: "Wheat Beer",
+    price: "$3.00",
+  }
+];
