4fc935041dbccb54dfd754876bc7f41050b48e3b
Estefanía <estefaniaramirezmnt@gmail.com>
2024-06-07 16:02:59 +0200

    Added handle error for images unavailable in BeerCard component

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import noImageAvailable from "../img/noImageAvailable.jpg";
+
 
 function BeerCard({ beer }) {
   const renderRatingStars = (rating) => {
@@ -10,13 +12,23 @@ function BeerCard({ beer }) {
     return stars;
   };
 
+  const handleImageError = (e) => {
+    e.target.src = noImageAvailable;
+  }
+
   return (
     <div className="selected-beer">
-      <img src={beer.image} alt={beer.name} />
+      <img
+        src={beer.image}
+        alt={beer.name}
+        onError={handleImageError} // Manejo del error de carga de la imagen
+      />{" "}
       <h3>{beer.name}</h3>
-      <p className="selected-beer-price"><span>Price:</span> {beer.price}</p>
+      <p className="selected-beer-price">
+        <span>Price:</span> {beer.price}
+      </p>
       <p className="selected-beer-rating">
-        <span>Rating:</span> {beer.rating.average.toFixed(2)} 
+        <span>Rating:</span> {beer.rating.average.toFixed(2)}
         {renderRatingStars(beer.rating.average)}
       </p>
     </div>
