0a9772560b78c7fd1ab397b8dd6800b20d93df69
Estefanía <estefaniaramirezmnt@gmail.com>
2024-06-07 11:44:50 +0200

    Added css for search input

--- src/components/FindYourBeer.js
+++ src/components/FindYourBeer.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function SearchBar() {
   return (
-    <div className='search-container'>
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
-        <h3>Find Your Beer</h3>
+    <div className="search-container">
+      <div className="input-container">
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="search-logo" />
+        <input
+          type="text"
+          placeholder="Search for a beer..."
+          className="search-bar"
+        />
+      </div>
+      <h3>Find Your Beer</h3>
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
