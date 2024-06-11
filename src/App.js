f197a244f6799e14e38a05dbb66704285cd77869
Estefanía <estefaniaramirezmnt@gmail.com>
2024-06-11 15:13:05 +0200

    Load Home with lazy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./components/Home";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-// import BeerList from "./components/BeerList";
 import Footer from "./components/Footer";
 import { ApiProvider } from "./components/ApiContext";
 import "./App.css";
 
+const Home = lazy(() => import("./components/Home"));
 const BeerList = lazy(() => import("./components/BeerList"));
 const FindYourBeer = lazy(() => import("./components/FindYourBeer"));
 
@@ -17,30 +16,37 @@ function App() {
     <div className="app-container">
       <div className="content-wrapper">
         <Header />
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/beerlist"
-              element={
-                <Suspense fallback={<div>Loading...</div>}>
-                  <ApiProvider>
-                    <BeerList />
-                  </ApiProvider>
-                </Suspense>
-              }
-            />
-            <Route
-              path="/findyourbeer"
-              element={
-                <Suspense fallback={<div>Loading...</div>}>
-                  <ApiProvider>
-                    <FindYourBeer />
-                  </ApiProvider>
-                </Suspense>
-              }
-            />
-          </Routes>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/beerlist"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <ApiProvider>
+                  <BeerList />
+                </ApiProvider>
+              </Suspense>
+            }
+          />
+          <Route
+            path="/findyourbeer"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <ApiProvider>
+                  <FindYourBeer />
+                </ApiProvider>
+              </Suspense>
+            }
+          />
+        </Routes>
       </div>
       <Footer />
     </div>
