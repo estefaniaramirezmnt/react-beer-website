e03365fa13627d64c0570b1f47d7f79015257e5a
Estefanía <estefaniaramirezmnt@gmail.com>
2024-06-11 11:57:22 +0200

    Created FindYourBeer.test.js and written const mockApiData

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import Header from "./components/Header";
@@ -17,7 +17,6 @@ function App() {
     <div className="app-container">
       <div className="content-wrapper">
         <Header />
-        <BrowserRouter>
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
@@ -42,7 +41,6 @@ function App() {
               }
             />
           </Routes>
-        </BrowserRouter>
       </div>
       <Footer />
     </div>
