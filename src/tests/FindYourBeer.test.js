03ec5eb8efb973877c1c4aa2422982751bd19678
Estefanía <estefaniaramirezmnt@gmail.com>
2024-06-11 12:12:31 +0200

    Added rating to mockApiData

--- src/tests/FindYourBeer.test.js
+++ src/tests/FindYourBeer.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import BeerList from "../components/BeerList";
+import FindYourBeer from "../components/FindYourBeer";
 import { ApiContext } from "../components/ApiContext";
 
 const mockApiData = [
@@ -10,6 +10,7 @@ const mockApiData = [
     image: "ale.jpg",
     alt: "Ale Beer",
     price: "$5.00",
+    rating: { average: 4.5 }
   },
   {
     id: 2,
@@ -17,6 +18,7 @@ const mockApiData = [
     image: "lager.jpg",
     alt: "Lager Beer",
     price: "$4.00",
+    rating: { average: 3.8 }
   },
   {
     id: 3,
@@ -24,6 +26,7 @@ const mockApiData = [
     image: "pilsner.jpg",
     alt: "Pilsner Beer",
     price: "$6.00",
+    rating: { average: 4.0 }
   },
   {
     id: 4,
@@ -31,6 +34,7 @@ const mockApiData = [
     image: "stout.jpg",
     alt: "Stout Beer",
     price: "$7.00",
+    rating: { average: 4.7 }
   },
   {
     id: 5,
@@ -38,5 +42,16 @@ const mockApiData = [
     image: "wheat.jpg",
     alt: "Wheat Beer",
     price: "$3.00",
+    rating: { average: 4.1 }
   },
 ];
+
+describe("FindYourBeer component", () => {
+  beforeEach(() => {
+    render(
+      <ApiContext.Provider value={{ read: () => mockApiData }}>
+        <FindYourBeer />
+      </ApiContext.Provider>
+    );
+  });
+});
